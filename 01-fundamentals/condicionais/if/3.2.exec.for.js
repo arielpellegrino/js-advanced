f5613f3d97c613c8e0037ab4e86d5b540b48de38
Ariel Pellegrino <ariel.pell@gmail.com>
2025-08-02 00:00:21 -0300

    exec 3

--- 01-fundamentals/condicionais/if/3.2.exec.for.js
+++ 01-fundamentals/condicionais/if/3.2.exec.for.js
@@ -1,14 +1,44 @@
-function checkPassword(pass){
-
+function checkPassword(pass) {
+    let isValid = null;
     const problemas = [];
+
     const checkSize = pass.length < 8 ? true : false;
     const checkUperCase = /[A-Z]/.test(pass);
     const checkLowerCase = /[a-z]/.test(pass);
     const checkNumber = /[0-9]/.test(pass);
     const checkSpecialCharacter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(pass);
-    return {
 
+    if (checkSize) {
+        problemas.push("Senha menor que 8 caracteres");
+    }
+
+    if (!checkUperCase) {
+        problemas.push("Pelo menos uma letra deve ser maiúscula");
+    }
+
+    if (!checkLowerCase) {
+        problemas.push("Pelo menos uma letra deve ser minúscula");
+    }
+
+    if (!checkNumber) {
+        problemas.push("A senha deve conter pelo menos 1 numero");
+    }
+
+    if (!checkSpecialCharacter) {
+        problemas.push("A senha deve conter pelo menos 1 caractere especial");
+    }
+
+    if (checkSize == true && checkUperCase == true && checkLowerCase == true && checkNumber == true && checkSpecialCharacter == true
+    ) {
+         isValid = true;
+    } else {
+        isValid = false
+    }
+
+    return {
+        válida: isValid,
+        problemas: problemas.join(" - ")
     }
 }
 
-console.log(checkPassword("Aa$a1aa")); 
+console.log(checkPassword("Aaa@a1aaa")); 
