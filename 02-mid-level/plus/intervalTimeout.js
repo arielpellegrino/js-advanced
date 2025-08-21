b2794a14f54e45dd6e43887f0a50ea119632b610
Ariel Pellegrino <ariel.pell@gmail.com>
2025-08-21 10:48:22 -0300

    fix: correct progress value in MarsCodeWorkspaceAppSettings and format mostraHora function

--- 02-mid-level/plus/intervalTimeout.js
+++ 02-mid-level/plus/intervalTimeout.js
@@ -1,7 +1,19 @@
-function mostraHora(){
+function mostraHora() {
     let data = new Date();
 
-    return data.toLocaleDateString('pt-BR',{
+    return data.toLocaleTimeString('pt-BR', {
         hour12: false
     });
 }
+
+//function funcaoDoInterval() {
+//    console.log(mostraHora());
+//}
+
+const timer = setInterval(()=>{
+    console.log(mostraHora());
+},1000)
+
+setTimeout(()=>{
+    clearInterval(timer);
+},10000)
